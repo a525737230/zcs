6174f08b8408a8382985dea9b7748705a4fbf3b8
Chason <525737230@qq.com>
2016-12-27 09:43:19 +0800

    text

---
+++ zcs/src/text/zcs.java
@@ -0,0 +1,5 @@
+package text;
+
+public class zcs {
+        int a;
+}
---
+++ zcs146/src/text/GitText.java
@@ -0,0 +1,8 @@
+package text;
+
+public class GitText {
+	public static void main(String args[]){ 
+		int j=1; 
+		System.out.println(j); 
+	}
+}
